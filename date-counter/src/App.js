6bf730b21abdcb1a92a7c23ccb5e31b962bdd281
Taavish Thaman <taavish@lal10.com>
2023-08-06 09:21:11 +0530

    add more stuff

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -2,61 +2,51 @@ import './index.css'
 import {useState} from 'react';
 
 function App() {
-  const [step, setStep] = useState(0);
-  const [count, setCount] = useState(0);
-  const [date, setDate] = useState(new Date(Date.now()).toDateString());
-
-  const decStep = () => {
-    setStep(s => s-1);
-    setDate((d) => {
-      return new Date(Date.now() + step * count * 24 * 60 * 60 * 1000).toDateString();
-    })
-  }
-
-  const incStep = () => {
-    setStep(s => s+1);
-    setDate((d) => {
-      return new Date(Date.now() + step * count * 24 * 60 * 60 * 1000).toDateString();
-    })
-  }
-
-  const decCount = () => {
-    setCount(c => c-1);
-    setDate((d) => {
-      return new Date(Date.now() + count * step * 24 * 60 * 60 * 1000).toDateString();
-    })
-  }
-
-  const incCount = () => {
-    setCount(c => c+1);
-    setDate((d) => {
-      return new Date(Date.now() + count * step * 24 * 60 * 60 * 1000).toDateString();
-    })
-  }
-
 
   return (
-
     <div className='center-div'>
-      <div>
-        <button className='btn' onClick={decStep}>-</button>
-        <p className='text'>Step: {step}</p>
-        <button className='btn' onClick={incStep}>+</button>
-      </div>
-      <div>
-        <button className='btn' onClick={decCount}>-</button>
-        <p className='text'>Count: {count * step}</p>
-        <button className='btn' onClick={incCount}>+</button>
-      </div>
-      <DateText days={count * step} date={date}></DateText>
+      <Counter/>
     </div>
   );
 }
 
-function DateText({days, date}) {
-  return <div className='dateText'>
-    <p>{days !== 0 ? `${Math.abs(days)} ${days > 0 ? ' days from today ' : ' days ago '} ` : 'Today '}is {date}</p>
-  </div>
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  function decreaseStep() {
+    setStep(s => s-1);
+  }
+
+  function increaseStep() {
+    setStep(s => s+1);
+  }
+
+  function decreaseCount() {
+    setCount(c => c-step);
+  }
+
+  function increaseCount() {
+    setCount(c => c+step);
+  }
+
+  return <>
+    <div>
+      <button className='btn' onClick={decreaseStep}>-</button>
+      <p className='text'>Step : {step}</p>
+      <button className='btn' onClick={increaseStep}>+</button>
+    </div>
+    <div>
+      <button className='btn' onClick={decreaseCount}>-</button>
+      <p className='text'>Count : {count}</p>
+      <button className='btn' onClick={increaseCount}>+</button>
+    </div>
+    <div className='date-text'>
+      {/* 6 days ago, 8 days from today */}
+      <p>{count !== 0 ? Math.abs(count) : 'Today'} {count !== 0 ? 'days' : ''} {count < 0 ? 'ago was' : 'from now is'} {new Date(new Date(date).setDate(new Date(date).getDate() + count)).toDateString()}</p>
+    </div>
+  </>
 }
 
 export default App;
