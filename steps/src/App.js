1c46f50227c10f80b40dfb71c141b6509ee9dc50
Taavish Thaman <taavish@lal10.com>
2023-08-07 12:41:49 +0530

    build accordian

--- steps/src/App.js
+++ steps/src/App.js
@@ -9,6 +9,14 @@ const messages = [
 export default function App() {
   return <div>
     <Steps/>
+    <StepMessage step={1}>
+      <p>Pass in content</p>
+      <p>✌🏾</p>
+    </StepMessage>
+    <StepMessage step={2}>
+      <p>Read children prop</p>
+      <p>😎</p>
+    </StepMessage>
   </div>
 }
 
@@ -35,18 +43,29 @@ function Steps() {
           <div className={step >= 2 ? 'active' : ''}>2</div>
           <div className={step >= 3 ? 'active' : ''}>3</div>
         </div>
-        <p className="message">Step : {step} : {messages[step-1]}</p>
+        {/* <p className="message">Step : {step} : {messages[step-1]}</p> */}
+        <StepMessage step={step}>
+          {messages[step-1]}
+          <div className='buttons'>
+            <Button bgColor={'#e7e7e7'} textColor={'#333'} onClick={() => {alert(`Learn how to ${messages[step-1]}`) }}>Learn How</Button>
+          </div>
+        </StepMessage>
         <div className="buttons">
-          <button 
-            style={{backgroundColor : '#7950f2', color : '#fff'}} 
-            onClick={handlePrevious}
-          >Previous</button>
-          <button 
-            style={{backgroundColor : '#7950f2', color : '#fff'}}
-            onClick={handleNext}>Next</button>
+          <Button textColor={'#fff'} bgColor={'#7950f2'} onClick={handlePrevious}><span>👈</span> Previous</Button>
+          <Button textColor={'#fff'} bgColor={'#7950f2'} onClick={handleNext}>Next <span>👉</span></Button>
         </div>
       </div>)
     }
   </>
   
+}
+
+function StepMessage({step, children}) {
+  return <div className="message"><h3>Step {step}</h3> {children}</div>
+}
+
+function Button({textColor, bgColor, onClick, children}) {
+  return <button 
+    style={{backgroundColor : bgColor, color : textColor}} onClick={onClick}
+  >{children}</button>
 }
